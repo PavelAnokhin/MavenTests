7d1a72f97af4724b1312af227571060fd774f531
PavelAnokhin <pav-anokhin@yandex.ru>
2021-05-26 19:49:41 +0300

    Commit for Maven Tests task

---
+++ src/test/java/org/example/service.java
@@ -0,0 +1,70 @@
+package org.example;
+
+import java.util.UUID;
+
+public class service {
+
+
+        private String name;
+        private String password;
+
+        public service (String name, String password) {
+            this.name = name;
+            this.password = password;
+        }
+
+
+}
+class MyEntity {
+
+    private UUID id;
+    private String name;
+
+    public MyEntity(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder().append("MyEntity{").append("id=").append(id).append(", name='").append(name).append('\'').append('}').toString();
+    }
+}
+class Db<DbSetting> {
+
+    private DbSetting dbSetting;
+    private MyEntity myEntity;
+
+    public Db(DbSetting dbSetting) {
+        this.dbSetting = dbSetting;
+        myEntity = new MyEntity("first");
+        myEntity.setId(UUID.randomUUID());
+    }
+
+    public DbSetting getDbSetting() {
+        return dbSetting;
+    }
+
+    public MyEntity getMyEntity() {
+        return myEntity;
+    }
+
+    public void setMyEntity(MyEntity myEntity) {
+        this.myEntity = myEntity;
+    }
+}
